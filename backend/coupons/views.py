0097a3f65cebb665bab2ab39a5e45d91ca3f6af4
Roger Anibal Zavarce de Armas <roger.zavarce@satec-es.com>
2022-07-18 11:23:51 +0200

    fea: test3 readey

--- backend/coupons/views.py
+++ backend/coupons/views.py
@@ -7,9 +7,6 @@ import os
 import json
 from .models import Coupon
 from .serializers import CouponSerializer
-from django.core import serializers
-from django.http import HttpResponse
-from rest_framework import viewsets
 
 from django_pandas.io import read_frame
 
@@ -39,28 +36,83 @@ class CouponsDataLoader(APIView):
 
         df = read_frame(qs)
 
-        # Using groupby() and count()
         group_by_type_counts = df.groupby(['promotion_type'])['promotion_type'].count().to_json()
 
         df_mask = df['promotion_type'] == 'percent-off'
-
         filtered_df = df[df_mask]
-
         percent_off_stats = {
             "counts": filtered_df['id'].count(),
-
+            "min": filtered_df['value'].min(),
+            "max": filtered_df['value'].max(),
+            "mean": round(filtered_df['value'].mean(), 2)
         }
 
+        df_mask = df['promotion_type'] == 'dollar-off'
+        filtered_df = df[df_mask]
+        dollar_off_stats = {
+            "counts": filtered_df['id'].count(),
+            "min": filtered_df['value'].min(),
+            "max": filtered_df['value'].max(),
+            "mean": round(filtered_df['value'].mean(), 2)
+        }
+
+        grouped_df = df.groupby(['webshop_id'])['value']
+        dollar_off_group_stats = {
+            "counts": grouped_df.count(),
+            "min": grouped_df.min(),
+            "max": grouped_df.max(),
+            "mean": grouped_df.mean(),
+        }
+
+        groups_stats = df.groupby(['webshop_id'])['value']
+        grouped_stats_list = {
+            "counts": json.loads(groups_stats.count().to_json()),
+            "min": json.loads(groups_stats.min().to_json()),
+            "max": json.loads(groups_stats.max().to_json()),
+            "mean": json.loads(groups_stats.mean().to_json()),
+        }
+
+        title_words_key = df[df.title.str.contains('10 Off|10% Off | 20 Off|20% Off', case=False)].to_dict()
+        title_words_key_list = []
+        for index, title in title_words_key['title'].items():
+            title_words_key_list.append(
+                {
+                    "id": index,
+                    "title": title
+                }
+            )
+
+        description_words_key = df[df.description.str.contains('10 Off|10% Off | 20 Off|20% Off', case=False)]
+
+        description_words_key_list = []
+        for index, description in description_words_key['description'].items():
+            description_words_key_list.append(
+                {
+                    "id": index,
+                    "description": description
+                }
+            )
+
         print()
-        print(percent_off_stats)
+        print(title_words_key)
+        #print(description_words_key)
         print()
+
+        words_keys_stats = {
+            "title": title_words_key_list,
+            "description": description_words_key_list,
+        }
 
         response = {
             "status": True,
             "payload": {
+                "totals": df.shape[0],
                 "group_by_promotion_type": json.loads(group_by_type_counts),
-                "pecent_off_stats": percent_off_stats
-
+                "percent_off_stats": percent_off_stats,
+                "dollar_off_stats": dollar_off_stats,
+                "dollar_off_group_stats": dollar_off_group_stats,
+                "grouped_stats_list": grouped_stats_list,
+                "words_keys_stats": words_keys_stats
             }
         }
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -13,10 +13,6 @@ class AuthGetToken(APIView):
     def post(self, request, *args, **kwargs):
 
         payload = request.data['payload']
-        print()
-        print(payload)
-        print()
-
         username = payload['username']
         password = payload['password']
         user = authenticate(request, username=username, password=password)
@@ -24,13 +20,6 @@ class AuthGetToken(APIView):
         if user is not None:
 
             token, _ = Token.objects.get_or_create(user=user)
-
-            print()
-            print(token)
-            print(user.pk)
-            print(user.email)
-            print()
-
             response = {
                 'token': str(token),
                 'user_id': user.pk,
@@ -38,13 +27,8 @@ class AuthGetToken(APIView):
                 'email': user.email
             }
 
-
         else:
             # Return an 'invalid login' error message.
-            print()
-            print("WWWWWWWWWWWWWWWWWW")
-            print()
-
             response = {
                 "status": False,
                 "msg": "Error: user and password are invalid."
